18d1f2f978f4805275987fc3e9e94a7f7a980940
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-07 17:46:38 +0900

    Information

--- src/prisma/migrations/20230107084622_init/migration.sql
+++ src/prisma/migrations/20230107084622_init/migration.sql
@@ -1,4 +1,11 @@
 -- CreateTable
+CREATE TABLE `Information` (
+    `content` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Information_content_key`(`content`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
