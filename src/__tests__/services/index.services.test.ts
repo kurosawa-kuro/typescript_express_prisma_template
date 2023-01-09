dcd1d7080c59e3509b7ad9c43db2fef4ff696348
Toshifumi Kurosawa <Kuromailserver@gmail.com>
2023-01-09 10:00:13 +0900

    c

--- src/__tests__/services/index.services.test.ts
+++ src/__tests__/services/index.services.test.ts
@@ -3,7 +3,7 @@ import { indexService } from "../../services/topService";
 describe("Top Service", () => {
     test("Top index service", async () => {
         const res = await indexService();
-        console.log('res', res)
+        // console.log('res', res)
         expect(res).toHaveProperty('message');
         expect(res).toEqual({ message: 'Hello World' });
     });
